4fd2bf8a11c559d87a3f74b5071fde1719bbd374
sujeeththan <sujeethsujeeth487@gmail.com>
2025-11-07 00:02:58 +0530

    seperate folder add

--- src/services/user.ts
+++ src/services/user.ts
@@ -3,6 +3,8 @@ import axios from "axios"
 const API_URL = "https://klick-jet-api.vercel.app/api/users"
 
 export const getUsers = async () => {
-    const res = await axios.get(API_URL)
+    console.log(API_URL);
+    const res = await axios .get(API_URL)
     return res.data.users
+    
 }
