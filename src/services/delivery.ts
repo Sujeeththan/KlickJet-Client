bca25a57bc1abc92e74b02d6a7813fa3f3773cdb
sujeeththan <sujeethsujeeth487@gmail.com>
2025-11-08 09:46:19 +0530

    deloivery data complete

--- src/services/delivery.ts
+++ src/services/delivery.ts
@@ -5,6 +5,8 @@ const API_URL = "https://klick-jet-api.vercel.app/api/delivery"
 export const getDeliveries = async () => {
     console.log(API_URL);
     const res = await axios .get(API_URL)
-    return res.data.delivery
+    console.log(res);
+    
+    return res.data.deliveries
     
 }
