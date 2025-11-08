a14a1c21834fc8d60df85334f3dd3ba4b41dbbf4
sujeeththan <sujeethsujeeth487@gmail.com>
2025-11-08 09:51:08 +0530

    payment table complete

--- src/services/payment.ts
+++ src/services/payment.ts
@@ -5,6 +5,8 @@ const API_URL = "https://klick-jet-api.vercel.app/api/payment"
 export const getPayments = async () => {
     console.log(API_URL);
     const res = await axios .get(API_URL)
-    return res.data.payment
+    console.log(res);
+    
+    return res.data.payments
     
 }
