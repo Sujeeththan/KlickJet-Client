c60e288a15d9aa8695d0a0bd110479bb595e0da8
sujeeththan <sujeethsujeeth487@gmail.com>
2025-11-08 09:54:34 +0530

    product table complete

--- src/services/product.ts
+++ src/services/product.ts
@@ -1,10 +1,12 @@
 import axios from "axios"
 
-const API_URL = "https://klick-jet-api.vercel.app/api/deliverer"
+const API_URL = "https://klick-jet-api.vercel.app/api/product"
 
 export const getProduct = async () => {
     console.log(API_URL);
     const res = await axios .get(API_URL)
-    return res.data.product
+    console.log(res);
+    
+    return res.data.products
     
 }
